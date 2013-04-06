22fdfcfc8a67fe9d2e824231e854b99b7e2c8db8
Mathías Montossi <mmontossi@mattways.com>
2013-04-06 15:40:17 -0300

    Small texts changes

--- lib/rails_pagination/railtie.rb
+++ lib/rails_pagination/railtie.rb
@@ -1,7 +1,7 @@
 module RailsPagination
   class Railtie < Rails::Railtie
 
-    config.pagination = ::ActiveSupport::OrderedOptions.new
+    config.pagination = ActiveSupport::OrderedOptions.new
     config.pagination.default_per_page = 30
     config.pagination.default_parameter = :p
     config.pagination.default_navigation = 5
