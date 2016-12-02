4a0227850d4026f006af538b4a18700b1a1971ec
Mathías Montossi <mmontossi@buyin.io>
2016-12-02 15:59:41 -0300

    Improve tests

--- test/view_test.rb
+++ test/view_test.rb
@@ -17,17 +17,19 @@ class ViewTest < ActionDispatch::IntegrationTest
     assert_select 'li.page a[href=?]', '/?page=5', text: /5/, count: 1
     assert_select 'li.next a[href=?]', '/?page=2', text: /Next/, count: 1
     assert_select 'li.last a[href=?]', '/?page=10', text: /Last/, count: 1
+    assert_select 'li.ellipsis', text: /…/, count: 1
 
-    get '/?page=3'
+    get '/?page=5'
     assert_select 'li.first a[href=?]', '/', text: /First/, count: 1
-    assert_select 'li.previous a[href=?]', '/?page=2', text: /Previous/, count: 1
-    assert_select 'li.page a[href=?]', '/', text: /1/, count: 1
-    assert_select 'li.page a[href=?]', '/?page=2', text: /2/, count: 1
-    assert_select 'li.page.current a[href=?]', '/?page=3', text: /3/, count: 1
+    assert_select 'li.previous a[href=?]', '/?page=4', text: /Previous/, count: 1
+    assert_select 'li.page a[href=?]', '/?page=3', text: /3/, count: 1
     assert_select 'li.page a[href=?]', '/?page=4', text: /4/, count: 1
-    assert_select 'li.page a[href=?]', '/?page=5', text: /5/, count: 1
-    assert_select 'li.next a[href=?]', '/?page=4', text: /Next/, count: 1
+    assert_select 'li.page.current a[href=?]', '/?page=5', text: /5/, count: 1
+    assert_select 'li.page a[href=?]', '/?page=6', text: /6/, count: 1
+    assert_select 'li.page a[href=?]', '/?page=7', text: /7/, count: 1
+    assert_select 'li.next a[href=?]', '/?page=6', text: /Next/, count: 1
     assert_select 'li.last a[href=?]', '/?page=10', text: /Last/, count: 1
+    assert_select 'li.ellipsis', text: /…/, count: 2
 
     get '/?page=10'
     assert_select 'li.first a[href=?]', '/', text: /First/, count: 1
@@ -39,6 +41,7 @@ class ViewTest < ActionDispatch::IntegrationTest
     assert_select 'li.page.current a[href=?]', '/?page=10', text: /10/, count: 1
     assert_select 'li.next', false
     assert_select 'li.last', false
+    assert_select 'li.ellipsis', text: /…/, count: 1
   end
 
 end
