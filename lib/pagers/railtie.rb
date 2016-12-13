9de00d32685132bcd460f7e8cf823e1abaef86af
Mathías Montossi <mmontossi@buyin.io>
2016-12-13 17:19:25 -0300

    Fix railtie

--- lib/pagers/railtie.rb
+++ lib/pagers/railtie.rb
@@ -2,7 +2,7 @@ module Pagers
   class Railtie < Rails::Railtie
 
     initializer 'pagers.active_record' do
-      ActiveRecord.on_load :active_record do
+      ActiveSupport.on_load :active_record do
         ::ActiveRecord::Base.include(
           Pagers::Extensions::ActiveRecord::Base
         )
@@ -10,7 +10,7 @@ module Pagers
     end
 
     initializer 'pagers.action_view' do
-      ActiveRecord.on_load :action_view do
+      ActiveSupport.on_load :action_view do
         ::ActionView::Base.include(
           Pagers::Extensions::ActionView::Base
         )
